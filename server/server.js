58b23bf56dfcbe1d73ede773619c7e4da8be4ea2
suregowtham123 <132137128+suregowtham123@users.noreply.github.com>
2026-03-15 01:09:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 
 const projectRoutes = require("./routes/projectRoutes");
 
@@ -11,41 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* MongoDB Connection */
-
-mongoose.connect(
-  process.env.MONGO_URI ||
-  "mongodb+srv://suregowthamkumar_db_user:12345@portfolio.nnzj0fs.mongodb.net/portfolio?retryWrites=true&w=majority"
-)
+mongoose.connect("mongodb+srv://suregowthamkumar_db_user:12345@portfolio.nnzj0fs.mongodb.net/portfolio?retryWrites=true&w=majority")
 .then(() => console.log("MongoDB Atlas Connected"))
 .catch(err => console.log(err));
 
-/* API Routes */
-
 app.use("/api/projects", projectRoutes);
 
-app.get("/api", (req,res)=>{
-  res.send("Portfolio Backend Running");
+app.get("/", (req,res)=>{
+    res.send("Portfolio Backend Running");
 });
 
-/* Serve React build in production */
+const PORT = 5000;
 
-if (process.env.NODE_ENV === "production") {
-
-  const rootPath = path.resolve();
-
-  app.use(express.static(path.join(rootPath, "../client/build")));
-
-  // Fallback route for React
-  app.use((req, res) => {
-    res.sendFile(path.join(rootPath, "../client/build/index.html"));
-  });
-}
-
-/* Port */
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, ()=>{
+    console.log("Server running on port 5000");
 });
